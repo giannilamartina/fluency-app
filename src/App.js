8056cfb4248ed5b559a91cd7b2b98c8d3c59e0a8
Gianni LaMartina <lamartinagianni@gmail.com>
2022-06-25 05:57:28 -0500

    fluency-app first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="main">
+          <div class="section" id="intro">
+            <h1>Fluency</h1>
+            <p>
+              Here at Fluency, we speak the languages that make your business
+              successful.<br></br>
+              We specialize in design, development, and everything in between.
+              <br></br>
+              Our mission is to guide you through an everchanging world by
+              bridging the gaps between tech and the real world.
+            </p>
+          </div>
+          <div class="section" id="services">
+            <h2>Services</h2>
+            <div class="card-container">
+              <div class="card">Digital Design</div>
+              <div class="card">Web Development</div>
+              <div class="card">Search Engine Optimization (SEO)</div>
+              <div class="card">Email Marketing</div>
+              <div class="card">Social Media Management</div>
+              <div class="card">Content Creation</div>
+            </div>
+            <div class="section" id="portfolio">
+              <h2>Portfolio</h2>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
